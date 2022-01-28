8dd431b6f5c5638a95ad61e87417a7232dd93b1e
wilsoiag <i.a.g.wilson@bham.ac.uk>
2022-01-28 20:58:13 +0000

    zoom 11 on mapload - good size for laptop

--- js/ENBS.js
+++ js/ENBS.js
@@ -5,7 +5,7 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/light-v10',
     // style: 'mapbox://styles/mapbox/streets-v11',
     center: [-1.8040, 52.4766],
-    zoom: 12,
+    zoom: 11,
 });
 
 const draw = new MapboxDraw({
